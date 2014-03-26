57ae8a08e2edbc075fb30ddc0ac8c745e7a33606
chgrl <christian.graul@gmail.com>
2014-03-26 14:16:18 +0100

    prepared for data subsets - corrected

--- R/frequency.R
+++ R/frequency.R
@@ -21,22 +21,6 @@ function(mast, v.set, dir.set, num.sectors=12, bins=c(5,10,15,20), subset, digit
 	if(is.null(mast$sets[[dir.set]]$data$dir.avg)) stop("Specified set does not contain wind direction data\n")
 	if(any(bins<0)) stop("'bins' must be NULL or a vector of positives\n")
 	
-	sector.width <- 360/num.sectors
-	sectors <- seq(0, 360-sector.width, by=sector.width)
-	sector.edges <- c(sectors-sector.width/2, tail(sectors, n=1)+sector.width/2)%%360
-	if(!is.null(bins)) if(head(bins, 1)!=0) bins <- c(0, bins)
-	num.classes <- length(bins)
-	v.max <- max(mast$sets[[v.set]]$data$v.avg, na.rm=TRUE)
-	if(num.classes>2) {
-		for(i in (num.classes-1):2) {
-			if(bins[i+1]>=v.max & bins[i]>=v.max) {
-				bins <- head(bins, -1)
-				num.classes <- length(bins)
-			}
-		}
-	}
-	if(!is.null(bins)) if(num.classes==2 && bins[num.classes]>=v.max) stop("Only one wind class found\n")
-	
 	# subset
 	num.samples <- length(mast$time.stamp)
 	if(missing(subset)) subset <- c(NA, NA)
@@ -57,32 +41,51 @@ function(mast, v.set, dir.set, num.sectors=12, bins=c(5,10,15,20), subset, digit
 	if(end<mast$time.stamp[1] || end>mast$time.stamp[num.samples]) stop("Specified 'end' not in period\n")
 	match.date <- difftime(mast$time.stamp, ISOdatetime(1,1,1,0,0,0), tz="GMT", units="days") - difftime(end, ISOdatetime(1,1,1,0,0,0), tz="GMT", units="days")
 	end <- which(abs(as.numeric(match.date)) == min(abs(as.numeric(match.date))))
+	v <- mast$sets[[v.set]]$data$v.avg[start:end]
+	d <- mast$sets[[dir.set]]$data$dir.avg[start:end]
+	
+	sector.width <- 360/num.sectors
+	sectors <- seq(0, 360-sector.width, by=sector.width)
+	sector.edges <- c(sectors-sector.width/2, tail(sectors, n=1)+sector.width/2)%%360
+	if(!is.null(bins)) if(head(bins, 1)!=0) bins <- c(0, bins)
+	num.classes <- length(bins)
+	v.max <- max(v, na.rm=TRUE)
+	if(num.classes>2) {
+		for(i in (num.classes-1):2) {
+			if(bins[i+1]>=v.max & bins[i]>=v.max) {
+				bins <- head(bins, -1)
+				num.classes <- length(bins)
+			}
+		}
+	}
+	if(!is.null(bins)) if(num.classes==2 && bins[num.classes]>=v.max) stop("Only one wind class found\n")
 		
 	freq.tbl <- matrix(NA, nrow=num.sectors+1, ncol=num.classes+2)
+	
 	# index for valid data
-	idx.val <- !is.na(mast$sets[[v.set]]$data$v.avg[start:end]) & !is.na(mast$sets[[dir.set]]$data$dir.avg[start:end]) & mast$sets[[v.set]]$data$v.avg[start:end]>=0
+	idx.val <- !is.na(v) & !is.na(d) & v>=0
 	
 	for(s in 1:num.sectors) {
 		# index for direction
 		low <- sector.edges[s]
 		high <- sector.edges[s+1]
-		if(low<high) idx.dir <- mast$sets[[dir.set]]$data$dir.avg[start:end]>=low & mast$sets[[dir.set]]$data$dir.avg[start:end]<high
-		else idx.dir <- mast$sets[[dir.set]]$data$dir.avg[start:end]>=low | mast$sets[[dir.set]]$data$dir.avg[start:end]<high
+		if(low<high) idx.dir <- d>=low & d<high
+		else idx.dir <- d>=low | d<high
 		
-		freq.tbl[s,1] <- mean(mast$sets[[v.set]]$data$v.avg[idx.val & idx.dir], na.rm=TRUE)
-		freq.tbl[s,2] <- length(mast$sets[[v.set]]$data$v.avg[idx.val & idx.dir]) * 100 / length(mast$sets[[dir.set]]$data$dir.avg[idx.val])
+		freq.tbl[s,1] <- mean(v[idx.val & idx.dir], na.rm=TRUE)
+		freq.tbl[s,2] <- length(v[idx.val & idx.dir]) * 100 / length(d[idx.val])
 		if(!is.null(bins)) {
 			for(c in 1:(num.classes-1)) {
 				# index for wind class
-				idx.class <- mast$sets[[v.set]]$data$v.avg[start:end]>=bins[c] & mast$sets[[v.set]]$data$v.avg[start:end]<bins[c+1]
-				freq.tbl[s,c+2] <- length(mast$sets[[v.set]]$data$v.avg[idx.val & idx.dir & idx.class]) * 100 / length(mast$sets[[dir.set]]$data$dir.avg[idx.val])
+				idx.class <- v>=bins[c] & v<bins[c+1]
+				freq.tbl[s,c+2] <- length(v[idx.val & idx.dir & idx.class]) * 100 / length(d[idx.val])
 			}
 		}
 		if(!is.null(bins)) {
-			freq.tbl[s,num.classes+2] <- length(mast$sets[[v.set]]$data$v.avg[idx.val & idx.dir & mast$sets[[v.set]]$data$v.avg[start:end]>=bins[num.classes]]) * 100 / length(mast$sets[[dir.set]]$data$dir.avg[idx.val])
+			freq.tbl[s,num.classes+2] <- length(v[idx.val & idx.dir & v>=bins[num.classes]]) * 100 / length(d[idx.val])
 		}
 	}
-	freq.tbl[num.sectors+1,1] <- mean(mast$sets[[v.set]]$data$v.avg[start:end], na.rm=TRUE) # idx.val instead of start:end?
+	freq.tbl[num.sectors+1,1] <- mean(v[idx.val], na.rm=TRUE) # idx.val???
 	freq.tbl[num.sectors+1,2] <- sum(freq.tbl[1:num.sectors,2], na.rm=TRUE)
 	
 	if(!is.null(bins)) for(i in 3:(num.classes+2)) freq.tbl[num.sectors+1,i] <- sum(freq.tbl[1:num.sectors,i], na.rm=TRUE)
