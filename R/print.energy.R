33b3018ce7c84656bc8cc7e7f02af8f0550027b5
chgrl <christian.graul@gmail.com>
2014-07-21 14:03:32 +0200

    (row.)names issue fixed

--- R/print.energy.R
+++ R/print.energy.R
@@ -2,9 +2,18 @@ print.energy <- function(x, ...) {
 ### summarising energy object information
 	
 	cat("\n\tWind energy content\n\n")
-	row.names(x) <- c(toupper(head(attr(x, "row.names"), -1)), tail(attr(x, "row.names"), 1))
-	x[x==0] <- ""
-	print(x, quote=FALSE)
+	
+	obj <- x[[1]]
+	if(length(x)>1) {
+		for(i in 2:length(x)) {
+			obj <- rbind(obj, x[[i]])
+		}
+	}	
+	obj <- as.data.frame(t(obj))
+	names(obj) <- names(x)
+	row.names(obj) <- c(toupper(head(attr(x, "row.names"), -1)), tail(attr(x, "row.names"), 1))
+	obj[obj==0] <- ""
+	print(obj, quote=FALSE)
 	cat("\t(all values in ", attr(x, "unit"), ")\n", sep="")
 	cat("\ncall: energy(wb=", attr(x, "call")$wb, ", rho=", attr(x, "call")$rho, ", bins=c(", paste(attr(x, "call")$bins, collapse=", "), "), digits=", attr(x, "call")$digits, ", print=", attr(x, "call")$print, ")\n\n", sep="")
 }
